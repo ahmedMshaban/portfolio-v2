57fdbb68da30f6766939ab8faaa70734c9a79ff3
ahmedMshaban <ahmed.ms.elias@gmail.com>
2022-05-29 15:08:32 +0300

    update my resume

--- src/components/menu.js
+++ src/components/menu.js
@@ -266,7 +266,7 @@ const Menu = () => {
               </ol>
             )}
 
-            <a href="/MyResume.pdf" className="resume-link">
+            <a href="https://ahmedmshaban.com/MyResume.pdf" className="resume-link">
               Resume
             </a>
           </nav>
--- src/components/nav.js
+++ src/components/nav.js
@@ -173,7 +173,7 @@ const Nav = ({ isHome }) => {
   );
 
   const ResumeLink = (
-    <a className="resume-button" href="/MyResume.pdf" target="_blank" rel="noopener noreferrer">
+    <a className="resume-button" href="https://ahmedmshaban.com/MyResume.pdf" target="_blank" rel="noopener noreferrer">
       Resume
     </a>
   );
